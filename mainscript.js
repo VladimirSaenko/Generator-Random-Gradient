b73c89342ddc96810e2bce2e9229947faa2daee2
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-02-15 23:39:33 +0200

    Update script

--- mainscript.js
+++ mainscript.js
@@ -4,6 +4,7 @@
 let gradientText = document.getElementById('gradient-text');
 let box = document.getElementById("box");
 let body = document.body;
+
 let directions = [
     "right",
     "left",
@@ -11,24 +12,27 @@ let directions = [
     "bottom"
 ];
 
+let createGradientButton = document.getElementById("createbtn");
+let copyButton = document.getElementById("copybtn");
+
 box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0, 4)]}` + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
 ',' + getRandomInt(0, 255) + ')' + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
 ',' + getRandomInt(0, 255) + ')' + ')';
 body.style.background = box.style.background;
 gradientText.value = box.style.background;
 
-function getRandomGradient() {
+createGradientButton.addEventListener('click', () => {
     box.style.background = 'linear-gradient(' + `to ${directions[getRandomInt(0, 4)]}` + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
     ',' + getRandomInt(0, 255) + ')' + ',' + 'rgb(' + getRandomInt(0, 255) + ',' + getRandomInt(0, 255) + 
     ',' + getRandomInt(0, 255) + ')' + ')';
     body.style.background = box.style.background;
     gradientText.value = box.style.background;
-}
+})
 
-function copyPassword() {
+copyButton.addEventListener('click', () => {
     gradientText.select();
     document.execCommand("copy");
-}
+})
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
