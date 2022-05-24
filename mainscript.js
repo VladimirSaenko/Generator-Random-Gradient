c4aade227c6fbe5c8f644ae2020cb62a42a67958
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-05-24 14:44:37 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -7,6 +7,13 @@ let body = document.body;
 let createGradientButton = document.getElementById("createbtn");
 let copyButton = document.getElementById("copybtn");
 
+// let directions = [
+//     "right",
+//     "left",
+//     "top",
+//     "bottom"
+// ];
+
 function getRandomGradient() {
     box.style.background = `linear-gradient(${getRandomInt(0, 350)}deg, rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}) 
     ${getRandomInt(0, 40)}%, rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}) ${getRandomInt(40, 100)}%)`;
